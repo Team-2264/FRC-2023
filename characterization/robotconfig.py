0c19a4cfbe5cd51742e663e5b478409c70bed217
Dhairya Gupta <dhairyag23@gmail.com>
2023-01-21 22:34:44 -0600

    thank you! thank you!!

---
+++ characterization/robotconfig.py
@@ -0,0 +1,32 @@
+{
+
+    "rightControllerTypes": ["TalonFX", "TalonFX"],
+    "leftControllerTypes": ["TalonFX", "TalonFX"],
+    # Note: The first id in the list of ports should be the one with an encoder
+    # Ports for the left-side motors
+    "leftMotorPorts": [50, 56],
+    # Ports for the right-side motors
+    "rightMotorPorts": [52, 54],
+    # Inversions for the left-side motors
+    "leftMotorsInverted": [False, False],
+    # Inversions for the right side motors
+    "rightMotorsInverted": [False, False],
+
+    # Wheel diameter (in units of your choice - will dictate units of analysis)
+    "wheelDiameter": 4,
+    # If your robot has only one encoder, set all right encoder fields to `None`
+    # Encoder edges-per-revolution (*NOT* cycles per revolution!)
+    # This value should be the edges per revolution *of the wheels*, and so
+    # should take into account gearing between the encoder and the wheels
+    "encoderEPR": 13824, # 7:1 gearing so 7*2048
+
+    # Whether the left encoder is inverted
+    "leftEncoderInverted": False,
+    # Whether the right encoder is inverted:
+    "rightEncoderInverted": False,
+
+    "gyroType": "Pigeon2",
+    "gyroPort": "15",
+}
+
+
--- src/main/java/frc/robot/autos/newPath.java
+++ src/main/java/frc/robot/autos/newPath.java
@@ -6,40 +6,61 @@ import frc.robot.subsystems.Swerve;
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 
 public class newPath extends SequentialCommandGroup {
-        public newPath(Swerve s_Swerve) {
+
+        Swerve s_Swerve;
+        PIDController thetaController;
+
+        public PPSwerveControllerCommand baseSwerveCommand(PathPlannerTrajectory trajectory) {
+                PPSwerveControllerCommand command = new PPSwerveControllerCommand(trajectory, s_Swerve::getPose,
+                                Constants.Swerve.swerveKinematics,
+                                new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+                                new PIDController(Constants.AutoConstants.kPYController, 0, 0), thetaController,
+                                s_Swerve::setModuleStates, s_Swerve);
+                return command;
+        }
+
+        public newPath(Swerve swerve) {
+                this.s_Swerve = swerve;
 
                 // This will load the file "Example Path.path" and generate it with a max
                 // velocity of 4 m/s and a max acceleration of 3 m/s^2
-                PathPlannerTrajectory exampleTrajectory = PathPlanner.loadPath("Diamond",
+                PathPlannerTrajectory exampleTrajectory = PathPlanner.loadPath("New New Path",
                                 new PathConstraints(Constants.AutoConstants.kMaxSpeedMetersPerSecond,
                                                 Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared));
 
-                var thetaController = new ProfiledPIDController(
-                                Constants.AutoConstants.kPThetaController, 0, 0,
-                                Constants.AutoConstants.kThetaControllerConstraints);
+                addRequirements(s_Swerve);
+
+                thetaController = new PIDController(
+                                Constants.AutoConstants.kPThetaController, 0, 0);
+
+                // Constants.AutoConstants.kThetaControllerConstraints);
                 thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
-                SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
-                                exampleTrajectory,
-                                s_Swerve::getPose,
-                                Constants.Swerve.swerveKinematics,
-                                new PIDController(Constants.AutoConstants.kPXController, 0, 0),
-                                new PIDController(Constants.AutoConstants.kPYController, 0, 0),
-                                thetaController,
-                                s_Swerve::setModuleStates,
-                                s_Swerve);
+                // SwerveControllerCommand swerveControllerCommand = new
+                // SwerveControllerCommand(
+                // exampleTrajectory,
+                // s_Swerve::getPose,
+                // Constants.Swerve.swerveKinematics,
+                // new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+                // new PIDController(Constants.AutoConstants.kPYController, 0, 0),
+                // thetaController,
+                // s_Swerve::setModuleStates,
+                // s_Swerve);
+                PPSwerveControllerCommand swerveControllerCommand2 = baseSwerveCommand(exampleTrajectory);
 
                 addCommands(
                                 new InstantCommand(() -> s_Swerve.resetOdometry(exampleTrajectory.getInitialPose())),
-                                swerveControllerCommand,
+                                swerveControllerCommand2,
                                 new InstantCommand(() -> System.out.println(s_Swerve.swerveOdometry.getPoseMeters())));
         }
 }
